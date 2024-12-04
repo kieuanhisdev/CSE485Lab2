faf140fec5ca0f9ed39856d17d9848cae406f270
Kieu Anh <kieuanh.dev@gmail.com>
2024-12-04 14:37:12 +0700

    connectdabase

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+
+function connectDatabase($severname, $username, $password, $dbname)
+{
+
+    try {
+        //tạo kết nối 
+        $conn = new PDO("mysql:host=$severname;dbname=$dbname", $username, $password);
+
+        //thiết lập chế độ báo lỗi
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //echo "kết nối thành công!";
+        return $conn;
+    } catch (PDOException $e) {
+        die("kết nối thất bại" . $e->getMessage());
+    }
+}
+2
+
+//thông tin kết nối đến cơ sở dữ liệu MySQL
+$severname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tintuc";
+
+$conn = connectDatabase($severname, $username, $password, $dbname);
+
+
+//đống chuỗi kết nối
+//$conn = null;
+?>
